b9597d9bab7fc0ed19fea2b2dc4668f30abe887b
Gurprem Bhullar <bhullargurprem@gmail.com>
2022-03-02 12:11:25 -0500

    started Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon,
+} from '@heroicons/react/solid'
+
+import bgImg from '../assets/carwash.jpg';
+
+function Hero() {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
+        
+    </div>
+  )
+}
+
+export default Hero
