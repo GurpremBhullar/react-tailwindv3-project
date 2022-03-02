436ea7563195eab29cb936a2283cbfc4f4e27cd4
Gurprem Bhullar <bhullargurprem@gmail.com>
2022-03-02 09:44:28 -0500

    add burgerIcon

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
+function Navbar() {
+  return (
+    <MenuIcon className='w-5'/>
+  )
+}
+
+export default Navbar
