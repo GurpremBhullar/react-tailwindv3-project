cdf18ffcff6b08ed9ae106746a26476f4b761633
Gurprem Bhullar <bhullargurprem@gmail.com>
2022-03-02 13:31:47 -0500

    made the hero section beautiful with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
 <>
  <Navbar />
+ <Hero />
  </>
  
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,12 +8,19 @@ import {
 
 import bgImg from '../assets/carwash.jpg';
 
-function Hero() {
+const Hero = () => {
   return (
     <div className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
-        
+        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+            <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+                <p className='text-2xl'>Fully Automated & Touchless Carwash</p>
+                <h1 className='py-3 text-5xl md:text-7xl font-bold'>Oil Change</h1>
+                <p className='text-2xl'>& Tire Shop</p>
+                <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
+            </div>
+        </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
